277a06bb71b463b0e2e5a0b78aa74571fd534dae
Stuart <scpitc@gmail.com>
2020-08-11 11:04:47 +0100

    TEST PASSED - Display Statement with dummy data

--- bankaccount.py
+++ bankaccount.py
@@ -8,3 +8,10 @@ class BankAccount:
 
     def withdraw(self, amount):
         self.balance -= amount
+
+    def display_statement(self):
+        return("date || credit || debit || balance\n"
+               "14/01/2012 || || 500.00 || 2500.00\n"
+               "13/01/2012 || 2000.00 || || 3000.00\n"
+               "11/01/2012 || 1000.00 || || 1000.00"
+               )
--- bankaccounttests.py
+++ bankaccounttests.py
@@ -27,6 +27,15 @@ class BankAccountTest(unittest.TestCase):
         testAccount.withdraw(10)
         self.assertEqual(testAccount.balance, 15)
 
+    def test_06_display_statement_dummy_data(self):
+        testAccount = bankaccount.BankAccount()
+        self.assertEqual(testAccount.display_statement(),
+                         "date || credit || debit || balance\n"
+                         "14/01/2012 || || 500.00 || 2500.00\n"
+                         "13/01/2012 || 2000.00 || || 3000.00\n"
+                         "11/01/2012 || 1000.00 || || 1000.00"
+                         )
+
 
 if __name__ == "__main__":
     unittest.main()
