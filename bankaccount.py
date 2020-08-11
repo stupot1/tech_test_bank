0943ae551205c493aaec9bfaec1f7291e12f1b1e
Stuart <scpitc@gmail.com>
2020-08-11 13:43:29 +0100

    Refactor - Created internal functions __add_transaction and __format_date

--- bankaccount.py
+++ bankaccount.py
@@ -9,35 +9,14 @@ class BankAccount:
 
     def deposit(self, amount, date=None):
         self.balance += amount
-        if date is None:
-            date = datetime.date.today()
-        else:
-            date = datetime.datetime.strptime(date, '%d/%m/%Y')
-        transaction_data = {
-            'date': date,
-            'amount': amount,
-            'balance': self.balance,
-            'type': "deposit"
-            }
-        self.transactions.append(transaction_data)
+        self.__add_transaction(self.__format_date(date), amount, "deposit")
 
     def withdraw(self, amount, date=None):
         self.balance -= amount
-        if date is None:
-            date = datetime.date.today()
-        else:
-            date = datetime.datetime.strptime(date, '%d/%m/%Y')
-        transaction_data = {
-            'date': date,
-            'amount': amount,
-            'balance': self.balance,
-            'type': "withdraw"
-            }
-        self.transactions.append(transaction_data)
+        self.__add_transaction(self.__format_date(date), amount, "withdraw")
 
     def display_statement(self):
         statement = "date || credit || debit || balance"
-
         for i in reversed(self.transactions):
             statement += ("\n")
             statement += (f"{i['date'].strftime('%d/%m/%Y')} || ")
@@ -46,5 +25,20 @@ class BankAccount:
             elif i['type'] == "deposit":
                 statement += (f"{'%.2f' % i['amount']} || || ")
             statement += (f"{'%.2f' % i['balance']}")
-
         return(statement)
+
+    def __add_transaction(self, date, amount, type):
+        transaction_data = {
+                            'date': date,
+                            'amount': amount,
+                            'balance': self.balance,
+                            'type': type
+                            }
+        self.transactions.append(transaction_data)
+
+    def __format_date(self, date):
+        if date is None:
+            return datetime.date.today()
+        else:
+            return datetime.datetime.strptime(date, '%d/%m/%Y')
+
